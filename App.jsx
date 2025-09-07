92ac7628ec61925da803f4e77f306d865d4966f4
udibh-frontegg <udi25cws@gmail.com>
2025-09-07 17:38:42 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
-import { useAuth, useLoginWithRedirect, AdminPortal } from '@frontegg/react';
+import './App.css';
+// import { useEffect } from 'react';
+import { useAuth, useLoginWithRedirect, ContextHolder } from "@frontegg/react";
 
 function App() {
   const { user, isAuthenticated } = useAuth();
@@ -11,22 +12,36 @@ function App() {
     }
   }, [isAuthenticated, loginWithRedirect]);
 
-  const handleSettingsClick = () => {
-    AdminPortal.show();
+  const logout = () => {
+    const baseUrl = ContextHolder.getContext().baseUrl;
+    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location.href}`;
   };
 
-  if (!isAuthenticated) return <div>Loading...</div>;
-
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome, {user?.name}</h1>
-      {user?.profilePicture && (
-        <img src={user.profilePicture} alt="Profile" style={{ borderRadius: '50%' }} />
+
+    <div className="App">
+      {isAuthenticated ? (
+        <div>
+          <div>
+            <img src={user?.profilePictureUrl} alt={user?.name} />
+          </div>
+          <div>
+            <span>Logged in as: {user?.name}</span>
+          </div>
+          <div>
+            <button onClick={() => alert(user.accessToken)}>
+              What is my access token?
+            </button>
+          </div>
+          <div>
+            <button onClick={() => logout()}>Click to logout</button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => loginWithRedirect()}>Click me to login</button>
+        </div>
       )}
-      <br />
-      <button onClick={handleSettingsClick} style={{ marginTop: '20px' }}>
-        Settings
-      </button>
     </div>
   );
 }
