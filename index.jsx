4ac94501d6976560f955ba706f3f614a5a8ab4fd
udibh-frontegg <udi25cws@gmail.com>
2025-09-07 17:56:43 +0300

    Remove index.css

--- index.jsx
+++ index.jsx
@@ -1,21 +1,18 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx'
-import './index.css';
-import { FronteggProvider } from '@frontegg/react';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { FronteggProvider } from "@frontegg/react";
 
 const contextOptions = {
-  baseUrl: 'https://app-grkb95gt40d2.frontegg.com',
-  clientId: '8a6a896a-f550-4853-97a2-5d79969cd9d3',
-  appId: 'd52557bd-5aa0-4adb-a090-4afd4c9284f4'
+  baseUrl: "https://app-grkb95gt40d2.frontegg.com",
+  clientId: "8a6a896a-f550-4853-97a2-5d79969cd9d3",
+  appId: "d52557bd-5aa0-4adb-a090-4afd4c9284f4"
 };
 
 const authOptions = {
-  keepSessionAlive: true // Uncomment this in order to maintain the session alive
+  keepSessionAlive: true
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
+ReactDOM.createRoot(document.getElementById("root")).render(
   <FronteggProvider
     contextOptions={contextOptions}
     hostedLoginBox={true}
@@ -23,4 +20,4 @@ root.render(
   >
     <App />
   </FronteggProvider>
-  , document.getElementById('root')); 
+);
